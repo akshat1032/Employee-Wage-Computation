506d211947c9df75b5e354cdddf424517d921232
akshat1032 <akshat.30oct@gmail.com>
2020-09-24 16:26:08 +0530

    Modified : Employee Wage Calculation Using Switch Case Modified

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -10,29 +10,32 @@ public class EmployeeWageComputation {
 	public static final int IS_PART_TIME = 1;
 	
 	public static void main(String[] args) {
-		
-		//Variable
-		double dailyWage = 0;
-		double partTimeWage = 0;
 			
-		//Storing Attendance
-		int empAttendance = (int)(Math.floor(Math.random() * 10) % 2);
+			//Variables
+			double dailyWage = 0;
+			double partTimeWage = 0;
 			
-		//Checking Attendance
-		if (empAttendance == 1) {
-			System.out.println("Employee present");
-			int empCheck = (int)(Math.floor(Math.random() * 10) % 2);
-			if (empCheck == IS_PART_TIME) {
-				partTimeWage = WAGE_PER_HOUR * PART_TIME_HOUR_PER_DAY;
-				System.out.println("Part Time Employee Daily Wage : "+partTimeWage);
+			//Storing Attendance
+			int empAttendance = (int)(Math.floor(Math.random() * 10) % 2);
+			
+			//Checking Attendance
+			switch(empAttendance) {
+				case 1:
+					System.out.println("Employee present");
+					int empCheck = (int)(Math.floor(Math.random() * 10) % 2);
+					switch(empCheck) {
+						case IS_FULL_TIME:
+							dailyWage = WAGE_PER_HOUR * HOUR_PER_DAY;
+							System.out.println("Full Time Employee Daily Wage : "+dailyWage);
+							break;
+						case IS_PART_TIME:
+							partTimeWage = WAGE_PER_HOUR * PART_TIME_HOUR_PER_DAY;
+							System.out.println("Part Time Employee Daily Wage : "+partTimeWage);
+							break;
+							}
+					break;
+				default:
+					System.out.println("Employee Absent");	
 				}
-			else {
-				dailyWage = WAGE_PER_HOUR * HOUR_PER_DAY;
-				System.out.println("Full Time Employee Daily Wage : "+dailyWage);
-				}
-			}
-		else {
-			System.out.println("Employee absent");
-			}
 		}
 }
