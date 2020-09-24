fa2b573246d6c584f8c572256ee5868db9c8a0be
akshat1032 <akshat.30oct@gmail.com>
2020-09-24 15:24:42 +0530

    Added : Welcome Message Added

---
+++ EmployeeWageComputation.java
@@ -0,0 +1,10 @@
+package com.capgemini.employeewagecomputation;
+
+public class EmployeeWageComputation {
+
+	public static void main(String[] args) {
+		
+		//Welcome message
+		System.out.println("Welcome to Employee Wage Computation Program");
+		}
+}
