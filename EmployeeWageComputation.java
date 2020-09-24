bf8feb56b9998b351f89e67ead9760b90a98c9a6
akshat1032 <akshat.30oct@gmail.com>
2020-09-24 17:52:47 +0530

    Added : Monthly Wage Calculation Upto Twenty Days Or Hundred Hours Added

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -4,6 +4,7 @@ public class EmployeeWageComputation {
 	
 	//Constants
 	public static final int WAGE_PER_HOUR = 20;
+	public static final int MAX_NO_OF_DAYS = 20;
 	public static final int HOUR_PER_DAY = 8;
 	public static final int PART_TIME_HOUR_PER_DAY = 4;
 	public static final int IS_FULL_TIME = 0;
@@ -16,35 +17,53 @@ public class EmployeeWageComputation {
 		double partTimeWage = 0;
 		double monthlyWage = 0;
 		double partTimeMonthlyWage = 0;
+		int workingHours = 0;
+		int workingDays = 0;
 		
-		for (int i = 0; i < 20; i++) {
-			
-			//Storing Attendance
-			int empAttendance = (int)(Math.floor(Math.random() * 10) % 2);
-			
-			//Checking Attendance
-			switch(empAttendance) {
-				case 1:
-					System.out.print("Employee present");
-					int empCheck = (int)(Math.floor(Math.random() * 10) % 2);
-					switch(empCheck) {
-						case IS_FULL_TIME:
-							System.out.print("(Full Time)\n"); //To show if full time employee is present
-							dailyWage = WAGE_PER_HOUR * HOUR_PER_DAY;
-							monthlyWage = monthlyWage + dailyWage;
-							break;
-						case IS_PART_TIME:
-							System.out.print("(Part Time)\n"); //To show if part time employee is present
-							partTimeWage = WAGE_PER_HOUR * PART_TIME_HOUR_PER_DAY;
-							partTimeMonthlyWage = partTimeMonthlyWage + partTimeWage;
-							break;
-						}
+		int empCheck = (int)(Math.floor(Math.random() * 10) % 2);
+		switch(empCheck) {
+			case IS_FULL_TIME:
+				while (workingHours<100 && workingDays<20) {
+								
+				//Storing Attendance
+				int empAttendance = (int)(Math.floor(Math.random() * 10) % 2);
+							
+				//Checking Attendance
+				switch(empAttendance) {
+					case 1:
+						dailyWage = WAGE_PER_HOUR * HOUR_PER_DAY;
+						monthlyWage = monthlyWage + dailyWage;
+						workingHours = workingHours + HOUR_PER_DAY;
+						workingDays = workingDays + 1;
 						break;
 					default:
-						System.out.println("Employee Absent");	
-					}
+						continue;
+						}
 				}
-		System.out.println("Full Time Employee Monthly Wage : "+monthlyWage);
-		System.out.println("Part Time Employee Monthly Wage : "+partTimeMonthlyWage);
+				System.out.println("No. of Days "+"  No. of Hours "+"  Monthly Wage "+"  Type of Employee");
+				System.out.println(workingDays +"		" + workingHours + "		" + monthlyWage+"		"+"Full Time Employee");
+			    break;
+			case IS_PART_TIME:
+				while (workingHours<100 && workingDays<20) {
+							
+				//Storing Attendance
+				int partTimeEmpAttendance = (int)(Math.floor(Math.random() * 10) % 2);
+								
+				//Checking Attendance
+				switch(partTimeEmpAttendance) {
+					case 1:
+						partTimeWage = WAGE_PER_HOUR * PART_TIME_HOUR_PER_DAY;
+						partTimeMonthlyWage = partTimeMonthlyWage + partTimeWage;
+						workingHours = workingHours + PART_TIME_HOUR_PER_DAY;
+						workingDays = workingDays + 1;
+						break;
+					default:
+						continue;
+						}
+					}
+				System.out.println("No. of Days "+"  No. of Hours "+"  Monthly Wage "+"  Type of Employee");
+				System.out.println(workingDays +"		" + workingHours + "		" + partTimeMonthlyWage+"		"+"Part Time Employee");
+				break;
+				}
 		}
 }
