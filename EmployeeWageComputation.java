9e78ffa8ee0336b77ec3a2be19e1b1fe23f7e94a
akshat1032 <akshat.30oct@gmail.com>
2020-09-24 15:51:42 +0530

    Added : Employee Daily Wage Calculation Added

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -2,17 +2,26 @@ package com.capgemini.employeewagecomputation;
 
 public class EmployeeWageComputation {
 
-	public static void main(String[] args) {
+	//Constants
+	public static final int WAGE_PER_HOUR = 20;
+	public static final int HOUR_PER_DAY = 8;
 		
-		//Storing Attendance
-		int empAttendance = (int)(Math.floor(Math.random() * 10) % 2);
-				
-		//Checking Attendance
-		if (empAttendance == 1) {
-			System.out.println("Employee present");
+	public static void main(String[] args) {
+			
+	//Variable
+	double dailyWage = 0;
+			
+	//Storing Attendance
+	int empAttendance = (int)(Math.floor(Math.random() * 10) % 2);
+			
+	//Checking Attendance
+	if (empAttendance == 1) {
+		System.out.println("Employee present");
+		dailyWage = WAGE_PER_HOUR * HOUR_PER_DAY;
 		}
-		else {
-			System.out.println("Employee absent");
-			}
+	else {
+		System.out.println("Employee absent");
+		}
+	System.out.println("Daily Wage : "+dailyWage);
 	}
 }
