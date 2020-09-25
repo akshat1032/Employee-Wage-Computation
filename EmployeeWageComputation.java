310c9cc6b29ce99c91380d50753f7039024733c8
akshat1032 <akshat.30oct@gmail.com>
2020-09-25 18:40:55 +0530

    Refactor : Monthly Wage Calculation Upto Twenty Days Or Hundred Hours Refactored

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -9,61 +9,73 @@ public class EmployeeWageComputation {
 	public static final int PART_TIME_HOUR_PER_DAY = 4;
 	public static final int IS_FULL_TIME = 0;
 	public static final int IS_PART_TIME = 1;
+	public static final int IS_PRESENT = 1;
+	public static final int IS_ABSENT = 0;
+	
+	//Variables
+	private double dailyWage = 0;
+	private double monthlyWage = 0;
+	private int workingDays = 0;
+	private int workingHours = 0;
+	
+	//Checking Type of Employee
+	public int checkEmployeeType() {
+		
+		int empCheck = (int)(Math.floor(Math.random() * 10) % 2);
+		return empCheck;
+	}
+	
+	//Checking Employee Attendance
+	public int checkEmployeeAttendance() {
+		
+		int empAttendance = (int)(Math.floor(Math.random() * 10) % 2);
+		return empAttendance;
+	}
+	
+	//Employee Wage Calculation
+	public double calculateEmployeeWage(int hourPerDay) {
+		
+		while (workingHours<100 && workingDays<20) {
+			
+			int empAttendance = this.checkEmployeeAttendance();
+			switch (empAttendance) {
+			case IS_PRESENT:
+				this.dailyWage = WAGE_PER_HOUR * hourPerDay;
+				this.monthlyWage = monthlyWage + dailyWage;
+				this.workingHours = this.workingHours + hourPerDay;
+				this.workingDays = this.workingDays + 1;
+				break;
+			case IS_ABSENT:
+				continue;
+			}
+		}
+		
+		return monthlyWage;
+	}
+	
+	//Printing the Employee Wage
+	public void printEmployeeWage(int empCheck) {
+		
+		double monthlyWage = 0;
+		String empType = "";
+		System.out.println("No. of Days  	"+"No. of Hours  	"+"Monthly Wage  	"+"Type of Employee");
+		switch(empCheck) {
+		case IS_FULL_TIME:
+			monthlyWage = this.calculateEmployeeWage(HOUR_PER_DAY);
+			empType = "Full Time";
+			break;
+		case IS_PART_TIME:
+			monthlyWage = this.calculateEmployeeWage(PART_TIME_HOUR_PER_DAY);
+			empType = "Part Time";
+			break;
+		}
+			System.out.println(this.workingDays+"		"+this.workingHours+"	        "+monthlyWage+"		"+empType);
+		}
 	
 	public static void main(String[] args) {
 			
-		//Variables
-		double dailyWage = 0;
-		double partTimeWage = 0;
-		double monthlyWage = 0;
-		double partTimeMonthlyWage = 0;
-		int workingHours = 0;
-		int workingDays = 0;
-		
-		int empCheck = (int)(Math.floor(Math.random() * 10) % 2);
-		switch(empCheck) {
-			case IS_FULL_TIME:
-				while (workingHours<100 && workingDays<20) {
-								
-				//Storing Attendance
-				int empAttendance = (int)(Math.floor(Math.random() * 10) % 2);
-							
-				//Checking Attendance
-				switch(empAttendance) {
-					case 1:
-						dailyWage = WAGE_PER_HOUR * HOUR_PER_DAY;
-						monthlyWage = monthlyWage + dailyWage;
-						workingHours = workingHours + HOUR_PER_DAY;
-						workingDays = workingDays + 1;
-						break;
-					default:
-						continue;
-						}
-				}
-				System.out.println("No. of Days "+"  No. of Hours "+"  Monthly Wage "+"  Type of Employee");
-				System.out.println(workingDays +"		" + workingHours + "		" + monthlyWage+"		"+"Full Time Employee");
-			    break;
-			case IS_PART_TIME:
-				while (workingHours<100 && workingDays<20) {
-							
-				//Storing Attendance
-				int partTimeEmpAttendance = (int)(Math.floor(Math.random() * 10) % 2);
-								
-				//Checking Attendance
-				switch(partTimeEmpAttendance) {
-					case 1:
-						partTimeWage = WAGE_PER_HOUR * PART_TIME_HOUR_PER_DAY;
-						partTimeMonthlyWage = partTimeMonthlyWage + partTimeWage;
-						workingHours = workingHours + PART_TIME_HOUR_PER_DAY;
-						workingDays = workingDays + 1;
-						break;
-					default:
-						continue;
-						}
-					}
-				System.out.println("No. of Days "+"  No. of Hours "+"  Monthly Wage "+"  Type of Employee");
-				System.out.println(workingDays +"		" + workingHours + "		" + partTimeMonthlyWage+"		"+"Part Time Employee");
-				break;
-				}
-		}
+		EmployeeWageComputation empWage = new EmployeeWageComputation();
+		int empCheck = empWage.checkEmployeeType();
+		empWage.printEmployeeWage(empCheck);
+	}
 }
