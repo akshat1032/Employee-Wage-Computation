878f12c1dca13eae6db524851da0112695e6da07
akshat1032 <akshat.30oct@gmail.com>
2020-09-28 17:58:25 +0530

    Refactor : Manage Monthly Wage For Multiple Companies Using Interface Refactored

--- EmployeeWageComputationMain.java
+++ EmployeeWageComputationMain.java
@@ -1,29 +1,43 @@
 package com.capgemini.employeewagecomputation;
 
+interface EmployeeWageComputationInterface{
+	
+	public void printEmployeeWage();
+}
+
+
+
 //Main function and creating objects for three companies with different data
-public class EmployeeWageComputationMain {
+public class EmployeeWageComputationMain implements EmployeeWageComputationInterface {
+	
+	public void printEmployeeWage() {
+		
+		//Creating array of objects to store details about multiple companies
+		EmployeeWageComputation empWageByCompany[] = new EmployeeWageComputation[3];
+				
+		int numberOfCompany = 0; //Counter for number of companies
+		int empCheck = 0;// Variable
+				
+		//Initializing value for three companies
+		empWageByCompany[0] = new EmployeeWageComputation("Company 1",20,20,100,8);
+		empWageByCompany[1] = new EmployeeWageComputation("Company 2",25,25,110,10);
+		empWageByCompany[2] = new EmployeeWageComputation("Company 3",30,30,120,12);
+				
+		while(numberOfCompany < 3) {
+
+			empCheck = empWageByCompany[numberOfCompany].checkEmployeeType();
+			empWageByCompany[numberOfCompany].printEmployeeWage(empCheck);
+			numberOfCompany++;
+				}
+	}
 	
 	public static void main(String[] args) {
 		
 		System.out.println("Company Name	"+"No. of Days  	"+"No. of Hours  	"+"Monthly Wage  	"+"Type of Employee");
 		
-		//Creating array of objects to store details aout multiple companies
-		EmployeeWageComputation empWageByCompany[] = new EmployeeWageComputation[3];
+		EmployeeWageComputationMain printObject = new EmployeeWageComputationMain();
+		printObject.printEmployeeWage();
 		
-		int numberOfCompany = 0; //Counter for number of companies
-		int empCheck = 0;// Variable
-		
-		//Initializing value for three companies
-		empWageByCompany[0] = new EmployeeWageComputation("Company 1",20,100,20,8);
-		empWageByCompany[1] = new EmployeeWageComputation("Company 2",25,110,25,10);
-		empWageByCompany[2] = new EmployeeWageComputation("Company 3",30,120,30,12);
-		
-		while(numberOfCompany < 3) {
-			
-			empCheck = empWageByCompany[numberOfCompany].checkEmployeeType();
-			empWageByCompany[numberOfCompany].printEmployeeWage(empCheck);
-			numberOfCompany++;
-		}
 	}
 
 }
